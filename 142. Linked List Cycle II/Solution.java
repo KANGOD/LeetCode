7fd710832c877f708e180c613dde55aae8d09393
Weekend <weekendgit@gmail.com>
2024-01-22 02:27:04 +0800

    142. Linked List Cycle II

---
+++ 142. Linked List Cycle II/Solution.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> hashSet = new HashSet<>();
+        ListNode cur = head;
+        while (cur != null) {
+            if (hashSet.contains(cur)) {
+                return cur;
+            } else {
+                hashSet.add(cur);
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ 142. Linked List Cycle II/Solution2.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ *
+ * https://www.bilibili.com/video/BV1if4y1d7ob/
+ *
+ * "fast 一定比 slow 多走了 k 步，这多走的 k 步其实就是 fast 指针在环里转圈圈，所以 k 的值就是环长度的「整数倍」。"
+ * via. https://labuladong.github.io/zgnb/5/31/
+ */
+public class Solution2 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                ListNode secondRun = head;
+                while (slow != secondRun) {
+                    slow = slow.next;
+                    secondRun = secondRun.next;
+                }
+                return secondRun;
+            }
+        }
+        return null;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
